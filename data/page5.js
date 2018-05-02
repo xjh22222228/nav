42dcf70688ade20f18ab417f030eaa3609ae4e6b
XieJiaHe <mb06@qq.com>
2018-05-02 22:24:09 +0800

    新增分类

---
+++ data/page5.js
@@ -0,0 +1,54 @@
+module.exports = {
+    title: '活到老，学到老',
+    hash: '#page5',
+    nav: [
+        {
+            icon: './icon/page5/icon-5x001.png',
+            name: '慕课网',
+            desc: '慕课网-程序员的梦工厂',
+            link: 'https://www.imooc.com/',
+        },
+        {
+            icon: './icon/page5/icon-5x002.png',
+            name: '极客学院',
+            desc: '致力于“让IT学习更有效”，以实战为特色，为注重自我提升的IT从业者提供高品质职业教育',
+            link: 'https://www.jikexueyuan.com/',
+        },
+        {
+            icon: './icon/page5/icon-5x003.svg',
+            name: '百度传课',
+            desc: '百度传课，中国教育领域新兴的在线教育平台',
+            link: 'https://chuanke.baidu.com/',
+        },
+        {
+            icon: './icon/page5/icon-5x004.jpg',
+            name: '腾讯课堂',
+            desc: '腾讯课堂 - 专业的在线教育平台',
+            link: 'https://ke.qq.com/index.html',
+        },
+        {
+            icon: './icon/page5/icon-5x005.png',
+            name: 'i春秋',
+            desc: '【i春秋】-专注网络安全_信息安全_白帽子的在线学习_教育_培训平台',
+            link: 'https://www.ichunqiu.com/',
+        },
+        {
+            icon: './icon/page5/icon-5x006.png',
+            name: '实验吧',
+            desc: '实验吧是国内最大的在线虚拟机实验IT教育平台',
+            link: 'http://www.shiyanbar.com/',
+        },
+        {
+            icon: './icon/page5/icon-5x007.png',
+            name: '合天网安实验室',
+            desc: '合天网安实验室是支持在线实验的信息安全学习的大型服务平台',
+            link: 'http://www.hetianlab.com/',
+        },
+        {
+            icon: './icon/page5/icon-5x008.png',
+            name: '网易云课堂',
+            desc: '网易云课堂 - 领先的实用技能学习平台',
+            link: 'http://study.163.com/',
+        },
+    ]
+}
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import angular from 'angular'
 import $ from 'jquery'
-import one from './data/one'
-import two from './data/two'
-import three from './data/three'
-import four from './data/four'
-import five from './data/five'
-import six from './data/six'
-import ten from './data/ten'
+import page1 from './data/page1'
+import page2 from './data/page2'
+import page3 from './data/page3'
+import page4 from './data/page4'
+import page5 from './data/page5'
+import page6 from './data/page6'
+import page7 from './data/page7'
+import page99 from './data/page99'
 
 !function(a,b,c){a.ripple=function(d,e){var f=this,g=f.log=function(){f.defaults.debug&&console&&console.log&&console.log.apply(console,arguments)};f.selector=d,f.defaults={debug:!1,on:"mousedown",opacity:.4,color:"auto",multi:!1,duration:.7,rate:function(a){return a},easing:"linear"},f.defaults=a.extend({},f.defaults,e);var h=function(b){var d,e,h=a(this);if(h.addClass("has-ripple"),e=a.extend({},f.defaults,h.data()),e.multi||!e.multi&&0===h.find(".ripple").length){if(d=a("<span></span>").addClass("ripple"),d.appendTo(h),g("Create: Ripple"),!d.height()&&!d.width()){var i=c.max(h.outerWidth(),h.outerHeight());d.css({height:i,width:i}),g("Set: Ripple size")}if(e.rate&&"function"==typeof e.rate){var j=c.round(d.width()/e.duration),k=e.rate(j),l=d.width()/k;e.duration.toFixed(2)!==l.toFixed(2)&&(g("Update: Ripple Duration",{from:e.duration,to:l}),e.duration=l)}var m="auto"==e.color?h.css("color"):e.color,n={animationDuration:e.duration.toString()+"s",animationTimingFunction:e.easing,background:m,opacity:e.opacity};g("Set: Ripple CSS",n),d.css(n)}e.multi||(g("Set: Ripple Element"),d=h.find(".ripple")),g("Destroy: Ripple Animation"),d.removeClass("ripple-animate");var o=b.pageX-h.offset().left-d.width()/2,p=b.pageY-h.offset().top-d.height()/2;e.multi&&(g("Set: Ripple animationend event"),d.one("animationend webkitAnimationEnd oanimationend MSAnimationEnd",function(){g("Note: Ripple animation ended"),g("Destroy: Ripple"),a(this).remove()})),g("Set: Ripple location"),g("Set: Ripple animation"),d.css({top:p+"px",left:o+"px"}).addClass("ripple-animate")};a(b).on(f.defaults.on,f.selector,h)}}($,document,Math);$.ripple.version = "1.2.1";
 
@@ -17,13 +18,14 @@ var app = angular.module('app', []);
 app.controller('controller', ['$scope', function ($scope) {
 	$scope.open = false;
 	$scope.nav = [
-		one, 
-		two,
-		three,
-		four,
-		five,
-		six,
-		ten,
+		page1, 
+		page2,
+		page3,
+		page4,
+		page5,
+		page6,
+		page7,
+		page99,
 	];
 	$scope.hash = function () {
 		var hash = window.location.hash.split('@')[0];
