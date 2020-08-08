d720704f6a5edbcad8b0be118f4f032f6f89061d
xjh22222228 <xjh22222228@gmail.com>
2020-08-08 21:16:48 +0800

    Update Data

--- data/utilityTool/gui.ts
+++ data/utilityTool/gui.ts
@@ -10,6 +10,12 @@ export default {
           desc: 'Typora是一款跨平台的最小Markdown编辑器，可为Markdown读者和作家提供无缝的体验',
           link: 'https://typora.io/',
         },
+        {
+          icon: 'https://www.iterm2.com/favicon.ico',
+          name: 'iterm2',
+          desc: 'iTerm2是Terminal的替代品，是iTerm的后续产品',
+          link: 'https://www.iterm2.com/',
+        },
       ]
     },
   ]
