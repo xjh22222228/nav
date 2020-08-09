3df938ed869258131a6b009d98e4569e7c616129
xjh22222228 <xjh22222228@gmail.com>
2020-08-09 22:18:40 +0800

    Update Data

--- data/utilityTool/gui.ts
+++ data/utilityTool/gui.ts
@@ -16,6 +16,12 @@ export default {
           desc: 'iTerm2是Terminal的替代品，是iTerm的后续产品',
           link: 'https://www.iterm2.com/',
         },
+        {
+          icon: 'https://www.parallels.cn/typo3conf/ext/prls_theme/Resources/Public/theme/res/img/favicon/favicon.png',
+          name: 'parallels',
+          desc: 'Mac和Windows虚拟化|管理Mac|VDI和RDS解决方案',
+          link: 'https://www.parallels.cn/',
+        },
       ]
     },
   ]
