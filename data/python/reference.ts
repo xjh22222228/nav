7c4127d83ad8d5016adc4a6431fbae24b311377f
xjh22222228 <xjh22222228@gmail.com>
2020-10-24 14:00:09 +0800

    Update Data

--- data/python/reference.ts
+++ data/python/reference.ts
@@ -3,6 +3,17 @@ export default {
   title: '参考资料',
   nav: [
     {
+      subtitle: '书籍',
+      nav: [
+        {
+          icon: 'https://www.python.org/static/favicon.ico',
+          name: '简明 Python 教程',
+          desc: '旨在于介绍如何使用 Python 语言进行编程的自由图书。它以教材与指南的形式为入门者介绍 Python 语言。如果你对电脑知识的了解仅限于如何保存文本文件的话，那这本书就是为你准备的。',
+          link: 'https://bop.mol.uno/',
+        },
+      ]
+    },
+    {
       subtitle: '文档资料',
       nav: [
         {
@@ -16,6 +27,12 @@ export default {
           name: 'Python 入门指南',
           desc: 'Python 入门指南',
           link: 'https://python666.cn/cls/lesson/list/',
+        },
+        {
+          icon: 'https://www.python.org/static/favicon.ico',
+          name: 'What the f*ck Python!',
+          desc: '一些有趣且鲜为人知的 Python 特性.',
+          link: 'https://github.com/satwikkansal/wtfpython',
         },
       ]
     },
