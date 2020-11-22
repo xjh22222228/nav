f771c00f98135649723231402d3b05fa2b465919
xjh22222228 <xjh22222228@gmail.com>
2020-11-22 09:13:24 +0800

    Update Data

--- data/frontend/react.ts
+++ data/frontend/react.ts
@@ -5,7 +5,6 @@ export default {
       title: '生态系统',
       nav: [
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'React',
           desc: '用于构建用户界面的 JavaScript 库',
           link: 'https://zh-hans.reactjs.org/',
@@ -16,7 +15,6 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/2.png',
           name: 'React Router',
           desc: 'React 路由管理',
           link: 'https://react-router.docschina.org/',
@@ -27,7 +25,7 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/3.png',
+          icon: 'https://avatars2.githubusercontent.com/u/13142323?s=200&v=4',
           name: 'Redux',
           desc: 'React 状态管理',
           link: 'http://www.redux.org.cn/docs/react-redux/api.html',
@@ -38,7 +36,6 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/4.png',
           name: 'create-react-app',
           desc: 'React官方脚手架',
           link: 'https://www.html.cn/create-react-app/',
@@ -54,7 +51,6 @@ export default {
       title: 'Redux相关',
       nav: [
         {
-          icon: 'assets/icon/frontend/3.png',
           name: 'React Redux',
           desc: 'Redux的官方React绑定(通常配合redux使用)',
           link: 'https://react-redux.js.org/',
@@ -65,7 +61,6 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/129.png',
           name: 'redux-actions',
           desc: 'Redux的Flux标准action实用程序(用于生成action)',
           link: 'https://redux-actions.js.org/',
@@ -76,13 +71,12 @@ export default {
           ]
         },
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'redux-thunk',
           desc: 'Redux的Thunk中间件, 用于处理异步逻辑',
           link: 'https://github.com/reduxjs/redux-thunk',
         },
         {
-          icon: 'assets/icon/frontend/130.png',
+          icon: 'https://avatars2.githubusercontent.com/u/24776643?s=200&v=4',
           name: 'redux-saga',
           desc: '一个旨在使应用程序副作用（即异步事物，如数据获取和不纯的东西，如访问浏览器缓存）更容易管理，执行更高效，易于测试，以及更好地处理故障的库',
           link: 'https://redux-saga.js.org/',
@@ -98,7 +92,6 @@ export default {
       title: 'React Native相关',
       nav: [
         {
-          icon: 'assets/icon/frontend/019.png',
           name: 'React Native',
           desc: '使用React构建原生APP程序的框架',
           link: 'https://reactnative.cn/docs/0.51/tutorial.html',
@@ -109,7 +102,7 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/117.png',
+          icon: 'https://avatars0.githubusercontent.com/u/29647600?s=200&v=4',
           name: 'React Navigation',
           desc: '为您的本地应用程序提供路由和导航',
           link: 'https://reactnavigation.org/zh-Hans/',
@@ -120,7 +113,7 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/118.png',
+          icon: 'https://avatars0.githubusercontent.com/u/12504344?s=200&v=4',
           name: 'expo',
           desc: '用于制作跨平台移动应用程序的Expo平台',
           link: 'https://docs.expo.io',
@@ -131,7 +124,7 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/5.png',
+          icon: 'https://avatars1.githubusercontent.com/u/12101536?s=200&v=4',
           name: 'ant-design-mobile-rn',
           desc: '一个基于 React Native 的 UI 组件库',
           link: 'https://rn.mobile.ant.design/index-cn',
@@ -147,7 +140,7 @@ export default {
       title: 'PC端UI组件库',
       nav: [
         {
-          icon: 'assets/icon/frontend/5.png',
+          icon: 'https://avatars1.githubusercontent.com/u/12101536?s=200&v=4',
           name: 'Ant Design',
           desc: 'Ant Design - A UI Design Language',
           link: 'https://ant.design/docs/react/introduce-cn',
@@ -169,7 +162,7 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/6.png',
+          icon: 'https://avatars2.githubusercontent.com/u/12810740?s=200&v=4',
           name: 'element',
           desc: 'elementUI组件库',
           link: 'https://elemefe.github.io/element-react/#/zh-CN/quick-start',
@@ -180,7 +173,7 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/032.png',
+          icon: 'https://avatars0.githubusercontent.com/u/6853419?s=200&v=4',
           name: 'react-bootstrap',
           desc: '基于Bootstrap 3构建的组件库',
           link: 'https://react-bootstrap.github.io/getting-started/introduction',
@@ -191,7 +184,6 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/087.png',
           name: 'evergreen',
           desc: 'Evergreen是一个React UI框架，用于在Web上构建雄心勃勃的产品。由Segment带给您。',
           link: 'https://evergreen.segment.com/',
@@ -202,7 +194,6 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/113.png',
           name: '@alifd/next',
           desc: '@alifd/next 是 Alibaba Fusion Design 的官方 React 实现, 赋能企业中后台产品开发',
           link: 'https://fusion.design/component/doc/102',
@@ -213,7 +204,7 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/093.png',
+          icon: 'https://avatars2.githubusercontent.com/u/11404085?s=200&v=4',
           name: 'zent',
           desc: 'Zent 是有赞 PC 端 WebUI 规范的 React 实现，提供了一整套基础的 UI 组件以及一些常用的业务组件',
           link: 'https://youzan.github.io/zent/zh/guides/install',
@@ -224,7 +215,6 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/126.png',
           name: 'React95',
           desc: '装修一新的现代的应用程序做出React Windows95的UI组件。',
           link: 'https://arturbien.github.io/React95',
@@ -235,7 +225,7 @@ export default {
           ]
         },
         {
-          icon: 'https://rsuitejs.com/favicon.ico',
+          icon: 'https://avatars1.githubusercontent.com/u/19635045?s=200&v=4',
           name: 'rsuite',
           desc: 'React Suite 是一套 React 组件库，为后台产品而生。同时也是一个具有贴心设计以及对开发者友好的 UI 框架',
           link: 'https://rsuitejs.com',
@@ -262,7 +252,7 @@ export default {
       title: '移动端UI组件库',
       nav: [
         {
-          icon: 'assets/icon/frontend/5.png',
+          icon: 'https://avatars1.githubusercontent.com/u/12101536?s=200&v=4',
           name: 'Ant Design Mobile',
           desc: '一个基于 Preact / React / React Native 的 UI 组件库',
           link: 'https://mobile.ant.design/index-cn',
@@ -278,7 +268,6 @@ export default {
       title: '其他组件库',
       nav: [
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'React Helmet',
           desc: '这个可重用的反应组件将管理对文档头的所有更改',
           link: 'https://github.com/nfl/react-helmet',
@@ -289,13 +278,11 @@ export default {
           ]
         },
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'react-image-crop',
           desc: '用于React的响应式图像裁剪工具',
           link: 'https://github.com/DominicTobias/react-image-crop',
         },
         {
-          icon: 'assets/icon/frontend/029.jpg',
           name: 'React Helmet',
           desc: '这个可重用的反应组件将管理对文档头的所有更改',
           link: 'https://github.com/nfl/react-helmet',
@@ -306,7 +293,6 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/030.png',
           name: 'react-transition-group',
           desc: '当React组件进入或离开DOM时执行动画的简单方法',
           link: 'https://reactcommunity.org/react-transition-group/',
@@ -317,19 +303,16 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/023.png',
           name: 'React Loadable',
           desc: '用于加载具有promise的组件的更高阶组件 - 路由懒加载',
           link: 'https://github.com/jamiebuilds/react-loadable',
         },
         {
-          icon: 'assets/icon/frontend/023.png',
           name: 'react-swipe',
           desc: '基于swipe.js封装的react组件/走马灯',
           link: 'https://github.com/voronianski/react-swipe',
         },
         {
-          icon: 'assets/icon/frontend/062.png',
           name: 'Ant Motion',
           desc: '使用 Ant Motion 能够快速在 React 框架中使用动画,我们提供了单项，组合动画，以及整套解决方案',
           link: 'https://motion.ant.design/',
@@ -340,8 +323,6 @@ export default {
           ]
         },
         {
-          icon: 'https://reactjs.org/favicon.ico',
-          name: 'video-react',
           desc: '网络视频播放器使用React库从头开始构建HTML5世界',
           link: 'https://video-react.js.org/',
           language: [
@@ -351,19 +332,16 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/095.png',
           name: 'react-beautiful-dnd',
           desc: '漂亮的和可访问的拖放列表',
           link: 'https://github.com/atlassian/react-beautiful-dnd',
         },
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'react-keeper',
           desc: '一个react路由库，更适合移动端使用，拥有缓存等功能',
           link: 'https://github.com/vifird/react-keeper',
         },
         {
-          icon: 'assets/icon/frontend/113.png',
           name: 'Fusion Design',
           desc: '基于React构建的Web可配置组件库',
           link: 'https://fusion.design',
@@ -374,7 +352,7 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/131.png',
+          icon: 'https://avatars0.githubusercontent.com/u/13690587?s=200&v=4',
           name: 'recharts',
           desc: '基于 React和D3 的组合式图表库',
           link: 'http://recharts.org/zh-CN/',
@@ -385,7 +363,6 @@ export default {
           ]
         },
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'react-json-view',
           desc: 'JSON查看器',
           link: 'https://mac-s-g.github.io/react-json-view/demo/dist/',
@@ -396,7 +373,6 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/013.png',
           name: 'BizCharts',
           desc: '基于G2和React的强大数据可视化库',
           link: 'https://bizcharts.net/products/bizCharts',
@@ -407,13 +383,11 @@ export default {
           ]
         },
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'react-draggable',
           desc: '一个简单的组件，用于使元素可拖动',
           link: 'https://github.com/mzabriskie/react-draggable',
         },
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'react-slick',
           desc: 'react走马灯组件',
           link: 'https://react-slick.neostack.com/',
@@ -424,7 +398,6 @@ export default {
           ]
         },
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'react-color',
           desc: 'react 拾色器',
           link: 'http://casesandberg.github.io/react-color/',
@@ -490,7 +463,7 @@ export default {
       title: '开箱即用',
       nav: [
         {
-          icon: 'assets/icon/frontend/033.png',
+          icon: 'https://avatars1.githubusercontent.com/u/12101536?s=200&v=4',
           name: 'ANT DESIGN PRO',
           desc: '开箱即用的中台前端/设计解决方案',
           link: 'https://pro.ant.design/',
@@ -506,7 +479,6 @@ export default {
       title: '单元测试',
       nav: [
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'enzyme',
           desc: '适用于React的JavaScript测试实用程序',
           link: 'https://enzymejs.github.io/enzyme/',
@@ -517,7 +489,6 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/115.png',
           name: 'Jest',
           desc: 'Jest是一个令人愉快的JavaScript测试框架，专注于简单性。',
           link: 'https://jestjs.io/zh-Hans/',
@@ -544,7 +515,7 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/030.png',
+          icon: 'https://avatars1.githubusercontent.com/u/20552239?s=200&v=4',
           name: 'DvaJS',
           desc: '基于React和Redux的轻量级和elm风格框架',
           link: 'https://dvajs.com/',
@@ -577,19 +548,16 @@ export default {
       title: '知识',
       nav: [
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'React.js 小书',
           desc: '这是一本关于 React.js 的小书',
           link: 'http://huziketang.mangojuice.top/books/react/',
         },
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'TypeScript-React-Starter',
           desc: 'TypeScript和React的入门模板，带有详细的README，描述了如何将两者结合使用。',
           link: 'https://github.com/Microsoft/TypeScript-React-Starter',
         },
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'react-redux-typescript-guide',
           desc: 'TypeScript中的React和 Redux - 使用说明',
           link: 'https://piotrwitek.github.io/react-redux-typescript-guide/',
@@ -605,7 +573,7 @@ export default {
       title: '其他',
       nav: [
         {
-          icon: 'assets/icon/frontend/034.png',
+          icon: 'https://avatars1.githubusercontent.com/u/17475736?s=200&v=4',
           name: 'Mobx',
           desc: '简单、可扩展的状态管理',
           link: 'https://cn.mobx.js.org/',
@@ -616,7 +584,6 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/030.png',
           name: 'next.js',
           desc: '服务端渲染框架、SEO解决方案',
           link: 'https://www.nextjs.cn/',
@@ -627,7 +594,7 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/122.png',
+          icon: 'https://avatars2.githubusercontent.com/u/25307599?s=200&v=4',
           name: 'react-dnd',
           desc: 'React DnD是一组React实用程序，可帮助您构建复杂的拖放接口，同时保持组件分离',
           link: 'https://react-dnd.github.io/react-dnd/about',
@@ -638,13 +605,11 @@ export default {
           ]
         },
         {
-          icon: 'assets/icon/frontend/030.png',
           name: 'nwb',
           desc: '适用于Web的React，Preact，Inferno和Vanilla JS应用，React库和其他npm模块的工具包，无需配置',
           link: 'https://github.com/insin/nwb',
         },
         {
-          icon: 'https://ahooks.js.org/simple-logo.svg',
           name: 'ahooks',
           desc: '为 React Hooks 而生',
           link: 'https://ahooks.js.org/zh-CN',
@@ -655,7 +620,6 @@ export default {
           ]
         },
         {
-          icon: 'https://reactjs.org/favicon.ico',
           name: 'react-app-rewired',
           desc: '覆盖create-react-app webpack配置而不弹出',
           link: 'https://github.com/timarney/react-app-rewired',
