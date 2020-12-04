43b06743fa47cd80c5c222152e24e71cc989f502
xjh22222228 <xjh22222228@gmail.com>
2020-12-04 22:08:42 +0800

    Update Data

--- data/go/package.ts
+++ data/go/package.ts
@@ -1,5 +1,6 @@
 export default {
   title: '依赖包',
+  icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
   nav: [
     {
       title: 'GUI',
@@ -21,7 +22,6 @@ export default {
       title: '日志模块',
       nav: [
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'logrus',
           desc: 'Go的结构化，可插入日志记录。',
           url: 'https://github.com/sirupsen/logrus',
@@ -38,7 +38,6 @@ export default {
       title: '数据库',
       nav: [
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'gorm',
           desc: '适用于Golang的绝佳ORM库，旨在对开发人员友好',
           url: 'https://gorm.io/zh_CN/',
@@ -60,19 +59,16 @@ export default {
           ]
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'mysql',
           desc: 'Go MySQL驱动程序是Go（golang）数据库/ sql包的MySQL驱动程序',
           url: 'https://github.com/go-sql-driver/mysql',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'sqlite3',
           desc: 'sqlite3驱动程序',
           url: 'https://github.com/mattn/go-sqlite3',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'pq',
           desc: '适用于database/sql的Pure Go Postgres驱动程序',
           url: 'https://github.com/lib/pq',
@@ -94,7 +90,6 @@ export default {
       title: '解析器',
       nav: [
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'json-iterator',
           desc: '高性能100％兼容的“ encoding / json”替代产品',
           url: 'http://jsoniter.com/migrate-from-go-std.html',
@@ -105,31 +100,26 @@ export default {
           ]
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'blackfriday',
           desc: 'Markdown处理器，安全、快速。',
           url: 'https://github.com/russross/blackfriday',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'goldmark',
           desc: '用Go语言编写的markdown解析器。易于扩展，符合标准（CommonMark），结构合理。',
           url: 'https://github.com/yuin/goldmark',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'goquery',
           desc: '接近jQuery语法的DOM解析库',
           url: 'https://github.com/PuerkitoBio/goquery',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'go-yaml',
           desc: 'yaml包使Go程序能够轻松地对yaml值进行编码和解码, 可以快速可靠地解析和生成YAML数据。',
           url: 'https://github.com/go-yaml/yaml/tree/v3',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'xurls',
           desc: '从文本中提取Urls',
           url: 'https://github.com/mvdan/xurls',
@@ -140,13 +130,11 @@ export default {
       title: '参数验证',
       nav: [
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'govalidator',
           desc: '参数验证程序，用于字符串，数字，切片和结构',
           url: 'https://github.com/asaskevich/govalidator',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'validator',
           desc: '基于标签对结构和各个字段实现值验证。',
           url: 'https://github.com/go-playground/validator',
@@ -157,31 +145,26 @@ export default {
       title: '其他',
       nav: [
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'ip',
           desc: '获取公网IP地址、v4/v6',
           url: 'https://github.com/xjh22222228/ip',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'terminal',
           desc: '提供了用于处理终端的支持功能，这在UNIX系统上很常见',
           url: 'https://godoc.org/golang.org/x/crypto/ssh/terminal',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'gopsutil',
           desc: '用于检索正在运行的进程和系统利用率（CPU，内存，磁盘，网络，传感器）的信息。它主要用于系统监视，概要分析和限制进程资源以及管理正在运行的进程',
           url: 'https://github.com/shirou/gopsutil',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'air',
           desc: 'Go热重载',
           url: 'https://github.com/cosmtrek/air',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'go-micro',
           desc: '分布式系统开发框架',
           url: 'https://go-micro.dev',
@@ -192,37 +175,31 @@ export default {
           ]
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'xlsx',
           desc: 'xlsx是一个库，用于简化在Go程序中读写Microsoft Excel最新版本使用的XML格式。',
           url: 'https://github.com/tealeg/xlsx',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'email',
           desc: 'Go强大而灵活的电子邮件库。',
           url: 'https://github.com/jordan-wright/email',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'websocket',
           desc: 'Go的一种快速，经过测试且广泛使用的WebSocket实现',
           url: 'https://github.com/gorilla/websocket',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'jwt-go',
           desc: 'Golang 实现的 JSON WEB TOKEN',
           url: 'https://github.com/dgrijalva/jwt-go',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'cron',
           desc: 'Go 实现的 cron 包',
           url: 'https://github.com/robfig/cron',
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'qrcode',
           desc: '二维码生成',
           url: 'https://github.com/skip2/go-qrcode',
@@ -239,7 +216,6 @@ export default {
           ]
         },
         {
-          icon: 'https://avatars1.githubusercontent.com/u/4314092?s=200&v=4',
           name: 'resty',
           desc: '用于Go的简单HTTP和REST客户端库',
           url: 'https://github.com/go-resty/resty',
@@ -265,6 +241,11 @@ export default {
             '',
             'https://github.com/gocolly/colly'
           ]
+        },
+        {
+          name: 'cors',
+          desc: '使用net / http可配置的处理程序来处理CORS请求',
+          url: 'https://github.com/rs/cors',
         },
       ]
     },
