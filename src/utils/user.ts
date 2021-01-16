c17109764c0d861ea134cb3cc3846d34a39a0c6d
xjh22222228 <xjh22222228@gmail.com>
2021-01-16 13:20:51 +0800

    init

--- src/utils/user.ts
+++
@@ -1,9 +0,0 @@
-// Copyright @ 2018-2021 xiejiahe. All rights reserved. MIT license.
-
-export function getToken() {
-  return window.localStorage.getItem('token')
-}
-
-export function setToken(token: string) {
-  return window.localStorage.setItem('token', token)
-}
