11008c1c64c95e6edbf4a5ee2a46b73d6c0de0df
xjh22222228 <xjh22222228@gmail.com>
2021-03-22 08:46:20 +0800

    feat: Add NProgress

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,6 +1,7 @@
 // Copyright @ 2018-2021 xiejiahe. All rights reserved. MIT license.
 
 import axios from 'axios'
+import NProgress from 'nprogress'
 import { getToken } from '../utils/user'
 
 const httpInstance = axios.create({
@@ -12,6 +13,8 @@ const token = getToken()
 Object.setPrototypeOf(httpInstance, axios)
 
 httpInstance.interceptors.request.use(function (config) {
+  NProgress.start()
+
   config.headers = {
     Authorization: `token ${token}`,
     ...config.headers
@@ -19,14 +22,16 @@ httpInstance.interceptors.request.use(function (config) {
 
   return config
 }, function (error) {
+  NProgress.done()
   return Promise.reject(error)
 })
 
 
 httpInstance.interceptors.response.use(function (res) {
-
+  NProgress.done()
   return res
 }, function (error) {
+  NProgress.done()
   return Promise.reject(error)
 })
 
