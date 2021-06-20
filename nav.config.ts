e2b4109a10f51f91fa1511c93ce5494054d10922
sy8tenn <56526282+sy8tenn@users.noreply.github.com>
2021-06-20 08:14:44 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -2,7 +2,7 @@ import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/sy8tenn/nav',
 
   // 部署分支
   branch: 'main',
@@ -14,13 +14,13 @@ const c: IConfig = {
   showGithub: true,
 
   // 您的网站地址，有利于SEO
-  homeUrl: 'https://nav3.cn',
+  homeUrl: 'https://sy8tenn.github.io/nav',
 
   // 网站标题
-  title: '发现导航 - 精选实用导航网站',
+  title: 'Security Tools',
 
   // 网站描述
-  description: '发现导航 - 精选实用导航网站',
+  description: '安全工具',
 
   // 网站关键字
   keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
