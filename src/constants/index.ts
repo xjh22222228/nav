87944af53cf721fed301ef9db483dafac158d03b
xjh22222228 <xjh22222228@gmail.com>
2021-06-21 13:21:38 +0800

    v5.3.3

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -15,7 +15,7 @@ export const LOGO_PATH = 'logo.png'
 
 export const LOGO_CDN = getCDN('logo.png')
 
-export const VERSION = '5.3.2'
+export const VERSION = '5.3.3'
 
 // keyboard event
 const prefix = isMac() ? 'command' : 'ctrl'
