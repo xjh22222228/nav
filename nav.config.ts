7674d7bdde207bf7d33d5b711970a096301edb0f
ziyuan <72785648+ajin21@users.noreply.github.com>
2021-09-06 21:42:15 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -2,7 +2,7 @@ import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/ajin21/nav',
 
   // 部署分支
   branch: 'main',
@@ -11,19 +11,19 @@ const c: IConfig = {
   hashMode: true,
 
   // 是否显示右上角的 Github 图标
-  showGithub: true,
+  showGithub: false,
 
   // 您的网站地址，有利于SEO
-  homeUrl: 'https://nav3.cn',
+  homeUrl: 'https:/ajin21.github.io',
 
   // 网站标题
-  title: '发现导航 - 精选实用导航网站',
+  title: '聚充站 - 精选实用导航网站',
 
   // 网站描述
   description: '发现导航 - 精选实用导航网站',
 
   // 网站关键字
-  keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全,node.js',
+  keywords: '导航,前端资源,社区站点,设计师,实用工具,学习资源,运营,网络安全',
 
   // 默认主题: Light | Sim | Side | App | Shortcut
   theme: 'Light',
