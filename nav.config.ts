b450c015ab1a0389cd693b19693a313b97de2f63
Joe <me@qjidea.com>
2023-07-21 15:33:17 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -3,11 +3,11 @@ import { IConfig } from './src/types'
 const c: IConfig = {
   // [Mondatory], Please replace following Github url with your own Github address which you forked in.
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/Usualminds/nav',
 
   // Deployment branch name
   // 部署分支
-  branch: 'main',
+  branch: 'feat-ads',
 
   // Whether the route is in Hash mode, if it is deployed on github pages, it must be set to true
   // 路由是否Hash模式, 如果是部署在github pages 务必设为 true
