f55ea49c8cf89cd7f334f79b4187f947d85878e6
Cheanerlia <137269991+Cheanerlia@users.noreply.github.com>
2023-09-20 15:56:38 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -2,7 +2,7 @@ import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [Mondatory], Please replace following Github url with your own Github address which you forked in.
-  // [必填], 请填写您的仓库地址?
+  // [必填], 请填写您的仓库地址
   gitRepoUrl: 'https://github.com/cheanerlia/nav',
 
   // Deployment branch name
