89d1e2afbe4ed487aaf81fe41bfbef11a5debf88
Hqyel <hye@stomtian.com>
2024-02-02 11:21:13 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -11,7 +11,7 @@ const c: IConfig = {
 
   // Whether the route is in Hash mode, if it is deployed on github pages, it must be set to true
   // 路由是否Hash模式, 如果是部署在github pages 务必设为 true
-  hashMode: false,
+  hashMode: true,
 }
 
 export default c
