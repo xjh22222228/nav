2d9eb1c7a98f5350b28170a4a36b3d27bae6ddcc
Hqyel <hye@stomtian.com>
2024-02-02 10:55:12 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -3,7 +3,7 @@ import { IConfig } from './src/types'
 const c: IConfig = {
   // [Mondatory], Please replace following Github url with your own Github address which you forked in.
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/Hqyel/nav',
 
   // Deployment branch name
   // 部署分支
@@ -11,7 +11,7 @@ const c: IConfig = {
 
   // Whether the route is in Hash mode, if it is deployed on github pages, it must be set to true
   // 路由是否Hash模式, 如果是部署在github pages 务必设为 true
-  hashMode: true,
+  hashMode: false,
 }
 
 export default c
