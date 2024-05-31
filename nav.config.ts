8a1b324d50ea6804e61b4c11cf306b2ad7039015
opensh <125738740+openMJJ@users.noreply.github.com>
2024-06-01 00:44:48 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -3,7 +3,7 @@ import { IConfig } from './src/types'
 const c: IConfig = {
   // [Mondatory], Please replace following Github url with your own Github address which you forked in.
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/openmjj/nav',
 
   // Deployment branch name
   // 部署分支
