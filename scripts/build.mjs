8a18a22b8bf792168ab5b0a7062f754c3d069880
xjh22222228 <xjh22222228@gmail.com>
2024-06-26 23:15:44 +0800

    fix: remove replace

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -78,7 +78,8 @@ async function build() {
   const writePath = path.join('.', 'src', 'index.html')
   let t = fs.readFileSync(htmlPath).toString()
   t = t.replace(/<title>.*<\/title>/i, '')
-  t = t.replace('<link rel="icon" href="assets/logo.png" id="x-jiahe" />', '')
+  t = t.replace('<link rel="icon" href="assets/logo.png" />', '')
+  t = t.replace('<link rel="icon" href="assets/logo.png">', '')
   t = t.replace('<!-- nav.config -->', htmlTemplate)
   if (headerContent) {
     t = t.replace('<!-- nav.headerContent -->', headerContent)
